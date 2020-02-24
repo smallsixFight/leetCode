6b78ed4e88992a6b66bd9f18b5dda3412a376eee
smallsixFight <smallsix.fight@gmail.com>
2020-02-25 00:16:55 +0800

    - submit MaxSubArray2();

--- maximum_subarray/maximum_subarray_test.go
+++ maximum_subarray/maximum_subarray_test.go
@@ -6,3 +6,8 @@ func TestMaxSubArray(t *testing.T) {
 	t.Log(MaxSubArray([]int{1, 2}))
 	t.Log(MaxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
 }
+
+func TestMaxSubArray2(t *testing.T) {
+	t.Log(MaxSubArray2([]int{1, 2}))
+	t.Log(MaxSubArray2([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
+}
