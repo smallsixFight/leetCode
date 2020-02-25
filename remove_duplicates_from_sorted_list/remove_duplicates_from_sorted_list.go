dd2f0effa46bdc4c9d44436f3850f3dcf2de5f82
smallsixFight <smallsix.fight@gmail.com>
2020-02-25 23:54:49 +0800

    - Update DeleteDuplicates() and add DeleteDuplicates2();

--- remove_duplicates_from_sorted_list/remove_duplicates_from_sorted_list.go
+++ remove_duplicates_from_sorted_list/remove_duplicates_from_sorted_list.go
@@ -5,11 +5,27 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func DeleteDuplicates(head *ListNode) *ListNode {
-	if head.Next == nil {
+// My Version
+func DeleteDuplicates2(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
 		return head
 	}
 	p1 := head.Next
+	for p1.Next != nil {
+		if p1.Val == p1.Next.Val {
+			p1.Next = p1.Next.Next
+		} else {
+			p1 = p1.Next
+		}
+	}
+	return head
+}
+
+func DeleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	p1 := head
 	for p1.Next != nil {
 		if p1.Val == p1.Next.Val {
 			p1.Next = p1.Next.Next
--- remove_duplicates_from_sorted_list/remove_duplicates_from_sorted_list_test.go
+++ remove_duplicates_from_sorted_list/remove_duplicates_from_sorted_list_test.go
@@ -2,6 +2,38 @@ package remove_duplicates_from_sorted_list
 
 import "testing"
 
+func TestDeleteDuplicates2(t *testing.T) {
+	head := &ListNode{}
+	n0 := ListNode{Val: 1}
+	n1 := ListNode{Val: 1}
+	n2 := ListNode{Val: 2}
+	head.Next = &n0
+	n0.Next = &n1
+	n1.Next = &n2
+	res := DeleteDuplicates2(head).Next
+	for res != nil {
+		t.Log(res.Val)
+		res = res.Next
+	}
+	t.Log("------------------------------------------")
+	n3 := ListNode{Val: 1}
+	n4 := ListNode{Val: 1}
+	n5 := ListNode{Val: 2}
+	n6 := ListNode{Val: 3}
+	n7 := ListNode{Val: 3}
+	head1 := &ListNode{}
+	head1.Next = &n3
+	n3.Next = &n4
+	n4.Next = &n5
+	n5.Next = &n6
+	n6.Next = &n7
+	res = DeleteDuplicates2(head1).Next
+	for res != nil {
+		t.Log(res.Val)
+		res = res.Next
+	}
+}
+
 func TestDeleteDuplicates(t *testing.T) {
 	head := ListNode{}
 	n0 := ListNode{Val: 1}
