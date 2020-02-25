738a1c092f014f8fe161e31854ce5266daed4961
smallsixFight <smallsix.fight@gmail.com>
2020-02-25 22:22:05 +0800

    - Update test;

--- climbing_stairs/climbing_strirs_test.go
+++ climbing_stairs/climbing_strirs_test.go
@@ -8,3 +8,10 @@ func TestClimbStairs(t *testing.T) {
 	t.Log(3 == ClimbStairs(3))
 	t.Log(5 == ClimbStairs(4))
 }
+
+func TestClimbStairs2(t *testing.T) {
+	t.Log(1 == ClimbStairs2(1))
+	t.Log(2 == ClimbStairs2(2))
+	t.Log(3 == ClimbStairs2(3))
+	t.Log(5 == ClimbStairs2(4))
+}
