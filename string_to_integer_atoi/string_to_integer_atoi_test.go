a3a069500a14fdd4202d5f194507c7e30c57d6db
smallsixFight <smallsix.fight@gmail.com>
2020-02-28 12:09:17 +0800

    - update test example;

--- string_to_integer_atoi/string_to_integer_atoi_test.go
+++ string_to_integer_atoi/string_to_integer_atoi_test.go
@@ -3,9 +3,9 @@ package string_to_integer_atoi
 import "testing"
 
 func TestMyAtoi(t *testing.T) {
-	//t.Log(MyAtoi("42"))
-	//t.Log(MyAtoi("   -42"))
+	t.Log(MyAtoi("42"))
+	t.Log(MyAtoi("   -42"))
 	t.Log(MyAtoi("4193 with words"))
-	//t.Log(MyAtoi("words and 987"))
-	//t.Log(MyAtoi("-91283472332"))
+	t.Log(MyAtoi("words and 987"))
+	t.Log(MyAtoi("-91283472332"))
 }
