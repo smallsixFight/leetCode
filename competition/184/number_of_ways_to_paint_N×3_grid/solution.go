01c3d3f85645254172fff832f26dfe71eec0ddcb
smallsixFight <smallsix.fight@gmail.com>
2020-04-12 23:46:00 +0800

    - add README.md and update solution.go;

--- competition/184/number_of_ways_to_paint_N×3_grid/solution.go
+++ competition/184/number_of_ways_to_paint_N×3_grid/solution.go
@@ -1,23 +1,5 @@
 package number_of_ways_to_paint_N_3_grid
 
-func NumOfWaysTwo(n int) int {
-	if n == 0 {
-		return 0
-	} else if n == 1 {
-		return 12
-	}
-	var temp = 1000000007
-	repeat := 6
-	unrepeat := 6
-	for i := 2; i <= n; i++ {
-		newrep := (repeat*3)%temp + unrepeat*2%temp
-		newunrep := repeat*2%temp + unrepeat*2%temp
-		repeat = newrep
-		unrepeat = newunrep
-	}
-	return (repeat + unrepeat) % temp
-}
-
 func NumOfWays(n int) int {
 	if n == 1 {
 		return 12
