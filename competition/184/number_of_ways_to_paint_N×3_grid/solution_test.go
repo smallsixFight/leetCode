a0d92c6bdecee9546313f03d0d8a64d53f2e7a07
smallsixFight <smallsix.fight@gmail.com>
2020-04-12 23:46:27 +0800

    - update solution_test.go;

--- competition/184/number_of_ways_to_paint_N×3_grid/solution_test.go
+++ competition/184/number_of_ways_to_paint_N×3_grid/solution_test.go
@@ -2,14 +2,8 @@ package number_of_ways_to_paint_N_3_grid
 
 import "testing"
 
-func TestNumOfWaysTwo(t *testing.T) {
-	t.Log(NumOfWaysTwo(3))
-	t.Log(NumOfWaysTwo(7))
-	t.Log(NumOfWaysTwo(5000))
-}
-
 func TestNumOfWays(t *testing.T) {
-	//t.Log(NumOfWays(3))
-	//t.Log(NumOfWays(7))
+	t.Log(NumOfWays(3))
+	t.Log(NumOfWays(7))
 	t.Log(NumOfWays(5000))
 }
