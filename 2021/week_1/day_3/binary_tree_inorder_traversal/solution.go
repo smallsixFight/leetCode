bb2cc7420387b433d4f62138ba81cd4ab3f4dc53
linwei <smallsix.fight@gmail.com>
2021-03-07 23:34:26 +0800

    add: 官方答案;

--- 2021/week_1/day_3/binary_tree_inorder_traversal/solution.go
+++ 2021/week_1/day_3/binary_tree_inorder_traversal/solution.go
@@ -58,3 +58,18 @@ func inorderTraversal2(root *TreeNode) []int {
 	return res
 
 }
+
+func inorderTraversal3(root *TreeNode) (res []int) {
+	stack := []*TreeNode{}
+	for root != nil || len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, root.Val)
+		root = root.Right
+	}
+	return
+}
--- 2021/week_1/day_3/binary_tree_inorder_traversal/solution_test.go
+++ 2021/week_1/day_3/binary_tree_inorder_traversal/solution_test.go
@@ -15,3 +15,13 @@ func Test_inorderTraversal2(t *testing.T) {
 	root := TreeNode{Val: 1, Right: n2}
 	t.Log(inorderTraversal2(&root))
 }
+
+func Test_inorderTraversal3(t *testing.T) {
+	n6 := &TreeNode{Val: 6}
+	n5 := &TreeNode{Val: 5}
+	n4 := &TreeNode{Val: 4}
+	n3 := &TreeNode{Val: 3, Left: n6}
+	n2 := &TreeNode{Val: 2, Left: n4, Right: n5}
+	root := TreeNode{Val: 1, Left: n2, Right: n3}
+	t.Log(inorderTraversal3(&root))
+}
